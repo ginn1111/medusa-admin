27839d5129b1151fb1ca114f6cda660cb15fa0ab
gin <vanthuanjw@gmail.com>
2023-08-03 01:19:40 +0700

    chore: add env

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,4 +1,5 @@
-const __MEDUSA_BACKEND_URL__ = "https://medusa-be-production.up.railway.app"
-const medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000"
+const medusaUrl =
+  process.env.MEDUSA_BACKEND_URL ||
+  "https://medusa-be-production.up.railway.app"
 
 export { medusaUrl }
